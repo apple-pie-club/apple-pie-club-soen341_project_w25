dec33edcfd8a183d741a21b1df17693522ab8b6a
Darcy <darcymccoy4@gmail.com>
2025-02-03 16:20:51 -0500

    Fix warning about deprecated code

--- my-chat-haven (passowrd hashing)/server.js
+++ my-chat-haven (passowrd hashing)/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json()); // For parsing application/json
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('MongoDB connection error:', err));
 
