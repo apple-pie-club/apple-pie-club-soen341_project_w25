2e437abb1ba4412ef36ac2a1e066b94158d25b78
Darcy <darcymccoy4@gmail.com>
2025-02-05 14:20:10 -0500

    Add isGlobalAdmin boolean to User

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-// Defining the user schema (structure of the data)
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isGlobalAdmin: { type: Boolean, required: true}
+}, { timestamps: true });
 
-// Exporting the model by first checking that it was already registred or no
 export default mongoose.models.User || mongoose.model("User", UserSchema);
