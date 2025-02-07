a068d0a86b3ef95cc39bd1d5d00dbe4a55221338
Sobhys <sobhyahmedyea@gmail.com>
2025-02-07 18:57:15 +0300

    User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isGlobalAdmin: { type: Boolean, required: true}
+    isGlobalAdmin: { type: Boolean, required: true } 
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
