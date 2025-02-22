0521459a92fde9ab8ba30059c9ef1044c7693a59
Darcy <darcymccoy4@gmail.com>
2025-02-22 14:39:01 -0400

    Delete favicon

--- chathaven/next.config.ts
+++ chathaven/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-  },
-  output: 'standalone',
-  experimental: {
-    forceSwcTransforms: true,
-  },
 };
 
 export default nextConfig;
