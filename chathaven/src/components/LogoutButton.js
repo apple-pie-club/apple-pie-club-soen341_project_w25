cffc7e5c55d9c9d78909047f1742c831f8c388f7
Marwa Hammani <hammanimarwa0@gmail.com>
2025-03-20 23:26:53 -0400

    set the user status to unavailable whenver the user logs out

--- chathaven/src/components/LogoutButton.js
+++ chathaven/src/components/LogoutButton.js
@@ -1,14 +1,29 @@
 import { useRouter } from "next/router";
 import "./styles/Dashboard.css";
+import { useSocket } from "./SocketContext";
 import { CiLogout } from "react-icons/ci";
 
 export default function LogoutButton() {
   const router = useRouter();
+  const { socket, userId, updateStatus } = useSocket();
 
   const handleLogout = async () => {
+    // Emit status change to "unavailable" before logging out
+    if (socket && userId) {
+      console.log("Setting status to 'unavailable' for user:", userId);
+      updateStatus("unavailable"); // Set the status to 'unavailable'
+    }
     await fetch("/api/auth/logout", { method: "POST", credentials: "include" });
     router.push("/login");
   };
 
-  return <button id="logoutButton" data-testid="logout-button" onClick={handleLogout}><CiLogout /></button>;
+  return (
+    <button
+      id="logoutButton"
+      data-testid="logout-button"
+      onClick={handleLogout}
+    >
+      <CiLogout />
+    </button>
+  );
 }
